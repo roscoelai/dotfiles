9c68b03da6733657d7bd8ae5bfef2df6818429f1
Roscoe <ahnouuyz@gmail.com>
2020-05-31 18:39:32 +0800

    Update setup_gitw64p.sh

--- setup_gitw64p.sh
+++ setup_gitw64p.sh
@@ -4,40 +4,54 @@
 # Setup script for 64-bit Git for Windows Portable
 
 initialize() {
-    # Create new home directory
     h="/home"
-    mkdir -p $h
-    mkdir -p "$h/bin"
-    mkdir -p "$h/packages"
-
-    # Create /etc/minttyrc
+    mkdir -p $h $h/bin $h/packages
     printf "Transparency=medium\nWindow=full" > /etc/minttyrc
-
-    # Backup and edit /etc/profile
     cp /etc/profile /etc/_profile
     sed -i "28i HOME=$h\nHOMEDRIVE=$h\nHOMEPATH=$h\nHISTFILE=$h\n" /etc/profile
 }
 
+m2xz() {
+    cd "$h/packages"
+    local fver=$1
+    local file="$fver-x86_64.pkg.tar.xz"
+    curl -O "http://repo.msys2.org/msys/x86_64/$file"
+    mkdir -p "$h/bin/$fver"
+    tar -xvf $file -C "$h/bin/$fver"
+    echo "$h/bin/$fver/usr/bin" >> "$h/.paths"
+}
+
+gh_clone() {
+    pkg=$1
+    dest=$2
+    mkdir -p $dest
+    git clone --depth=1 "https://github.com/$pkg" $dest
+    rm -rf "$dest/.git"
+}
+
+gh_raw() {
+    repo=$1
+    file=$2
+    dest=$3
+    mkdir -p $dest
+    curl "https://raw.githubusercontent.com/$repo/$file" > "$dest/$file"
+}
+
 downloads() {
-    # Download dotfiles
     cd $h
-    url="https://raw.githubusercontent.com/roscoelai/dotfiles/master"
-    curl -O "$url/.vimrc"
-    curl -O "$url/.bashrc"
+
+    # Download dotfiles
+    gh_raw "roscoelai/dotfiles/master" ".vimrc" $h
+    gh_raw "roscoelai/dotfiles/master" ".bashrc" $h
 
     # Download vim-polyglot
-    fld="$h/.vim/pack/default/start"
-    mkdir -p $fld
-    git clone "https://github.com/sheerun/vim-polyglot" "$fld/vim-polyglot"
+    gh_clone "sheerun/vim-polyglot" "$h/.vim/pack/default/start/vim-polyglot"
 
     # Download rsync
-    cd "$h/packages"
-    ver="rsync-3.1.3-1"
-    file="$ver-x86_64.pkg.tar.xz"
-    curl -O "http://repo.msys2.org/msys/x86_64/$file"
-    mkdir -p "$h/bin/$ver"
-    tar -xvf $file -C "$h/bin/$ver"
-    echo "$h/bin/$ver/usr/bin" >> "$h/.paths"
+    m2xz "rsync-3.1.3-1"
+
+    # Download tree
+    m2xz "tree-1.8.0-1"
 }
 
 # initialize
@@ -48,13 +62,19 @@ logout
 
 
 ## Others
+
+# ext=${file##*.}
+
+# Download zstd
+# fver="zstd-v1.4.5"
+# file="$fver-win64.zip"
+# curl -LO "https://github.com/facebook/zstd/releases/download/v1.4.5/$file"
+# mkdir -p "$h/bin/$fver"
+# unzip $file -d "$h/bin/$fver"
+# echo "$h/bin/$fver" >> "$h/.paths"
+
 # Download Miniconda3 Windows 64-bit
 # curl -LO "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
-
-# Download vim-code-dark
-# fld="$h/.vim/pack/default/start"
-# mkdir -p $fld
-# git clone "https://github.com/tomasiser/vim-code-dark" "$fld/vim-code-dark"
 
 # Download java
 # ver="13.0.2-14"
@@ -72,28 +92,27 @@ logout
 # unzip $file -d "$h/bin/lua"
 # echo "$h/bin/lua" >> "$h/.paths"
 
+# Download vim-code-dark
+# fld="$h/.vim/pack/default/start"
+# pkg="vim-code-dark"
+# mkdir -p $fld
+# git clone "https://github.com/tomasiser/$pkg" "$fld/$pkg"
+
+## Techinically challenging
+
+# Download tmux
+# fver="tmux-3.1-1"
+# file="$fver-x86_64.pkg.tar.zst"
+# curl -O "http://repo.msys2.org/msys/x86_64/$file"
+# mkdir -p "$h/bin/$fver"
+# tar -xvf $file -C "$h/bin/$fver"
+# echo "$h/bin/$fver/usr/bin" >> "$h/.paths"
+
+## Obsolete
+
 # Download tcc
 # file="tcc-0.9.27-win64-bin.zip"
 # curl -O "http://download.savannah.gnu.org/releases/tinycc/$file"
 # mkdir -p "$h/bin/tcc"
 # unzip $file -d "$h/bin/tcc"
 # echo "$h/bin/tcc" >> "$h/.paths"
-
-# download() {
-#     local url=$1
-#     local file=$2
-#     local dest=$3
-#     local path=$4
-#     local method=$5
-#     cd "$h/packages"
-#     curl -LO "$url/$file"
-#     mkdir -p $dest
-#     if [[ $method -eq "tar" ]]; then
-#         tar -xvf $file -C $dest
-#     elif [[ $method -eq "unzip" ]]; then
-#         unzip $file -d $dest
-#     elif [[ $method -eq "7z" ]]; then
-#         7z x $file -o$dest
-#     fi
-#     echo $path >> "$h/.paths"
-# }
